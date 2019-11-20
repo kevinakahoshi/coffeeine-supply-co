52784d206bdfb994aca580113ae02913e1494886
kevinakahoshi <kevin.akahoshi@gmail.com>
2019-11-20 04:52:52 +0000

    completed insert for timestamp

--- server/api/cart.php
+++ server/api/cart.php
@@ -1,7 +1,7 @@
 <?php
+$link = get_db_link();
 
 if ($request['method'] === 'GET') {
-  $link = get_db_link();
   $sessionData = isset($_SESSION[`cart_id`]);
   if (!$sessionData) {
     $response['body'] = [];
@@ -10,5 +10,17 @@ if ($request['method'] === 'GET') {
 }
 
 if ($request['method'] === 'POST') {
-  return null;
+  // $productId = $request['body']['productId'];
+  // $checkProductId = isset($request['body']['productId']);
+  // if (!$checkProductId) {
+  //   throw new ApiError('That is not a valid product ID', 400);
+  // }
+  // $query = "SELECT * FROM products WHERE productId=1";
+
+  $inserted_time = "INSERT INTO carts (createdAt) VALUES (CURRENT_TIMESTAMP)";
+  $link->query($inserted_time);
+  $insert_id = $link->insert_id;
+
+  $response['body'] = $insert_id;
+  send($response);
 }
