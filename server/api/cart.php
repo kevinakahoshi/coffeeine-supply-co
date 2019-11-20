0b4aa5d29affdf75ab2894c662defc679cb9efa4
kevinakahoshi <kevin.akahoshi@gmail.com>
2019-11-20 20:51:32 +0000

    returned the correct data that was joined in PHP MyAdmin

--- server/api/cart.php
+++ server/api/cart.php
@@ -10,17 +10,25 @@ if ($request['method'] === 'GET') {
 }
 
 if ($request['method'] === 'POST') {
-  // $productId = $request['body']['productId'];
-  // $checkProductId = isset($request['body']['productId']);
-  // if (!$checkProductId) {
-  //   throw new ApiError('That is not a valid product ID', 400);
-  // }
-  // $query = "SELECT * FROM products WHERE productId=1";
-
+  $product_id = $request['body']['productId'];
+  $check_product_id = isset($product_id);
+  if (!$check_product_id) {
+    throw new ApiError('That is not a valid product ID', 400);
+  }
+  $select_product = "SELECT * FROM products WHERE productId=$product_id";
+  $product_query = $link->query($select_product);
+  $product_info = mysqli_fetch_assoc($product_query);
+  $product_price = $product_info['price'];
   $inserted_time = "INSERT INTO carts (createdAt) VALUES (CURRENT_TIMESTAMP)";
-  $link->query($inserted_time);
+  $time_query = $link->query($inserted_time);
   $insert_id = $link->insert_id;
+  $cart_items = "INSERT INTO cartItems (cartId, productId, price) VALUES ($insert_id, $product_id, $product_price)";
+  $cart_query = $link->query($cart_items);
+  $cart_info = mysqli_fetch_assoc($cart_query);
+  $joined_cart = "SELECT cartItems.cartId AS id, cartItems.productId, products.name, products.price, products.image, products.shortDescription FROM cartItems JOIN carts ON cartItems.cartId = carts.cartId JOIN products ON cartItems.productId = products.productId WHERE cartItems.cartId=$insert_id";
+  $joined_query = $link->query($joined_cart);
+  $joined_info = mysqli_fetch_assoc($joined_query);
 
-  $response['body'] = $insert_id;
+  $response['body'] = $joined_info;
   send($response);
 }
