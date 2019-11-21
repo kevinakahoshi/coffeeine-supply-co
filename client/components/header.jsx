cb2922a75a0f750101557ea8014c481aa86d5c98
kevinakahoshi <kevin.akahoshi@gmail.com>
2019-11-21 17:51:42 +0000

    Wicked Sales | Add to Cart Front End - Kevin Akahoshi

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 function Header(props) {
   const name = props.name;
+  const itemCount = props.cartItems.length;
   return (
     <div className="bg-dark mb-5 p-3">
       <div className="container">
-        <h5 className="text-light m-0"><i className="fas fa-dollar-sign"></i> {name}</h5>
+        <h5 className="text-light d-inline-block m-0"><i className="fas fa-dollar-sign"></i> {name}</h5>
+        <p className="text-light d-inline-block float-right m-0">{itemCount} Items <i className="fas fa-shopping-cart"></i></p>
       </div>
     </div>
   );
