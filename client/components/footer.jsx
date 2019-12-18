39789fb70a4ba57ff14b143d22b9979b3fcce598
kevinakahoshi <kevin.akahoshi@gmail.com>
2019-12-18 17:13:08 +0000

    Added a min-height to a div that holds content in the middle of the page

--- client/components/footer.jsx
+++ client/components/footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <div className="bg-dark py-5 mt-3">
+    <div className="bg-dark py-5">
       <div className="container">
         <h3 className="text-center text-white"><i className="fas fa-mug-hot" /> Coffeine Supply Company</h3>
         <h6 className="text-center text-white">An Akahoshi Heavy Industries Brand</h6>
