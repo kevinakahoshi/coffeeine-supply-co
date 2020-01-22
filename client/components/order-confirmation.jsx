43400f04771ae7ac9fe1560df5ded6c4b98ff219
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2020-01-22 20:04:34 +0000

    Forgot confirmation page

--- client/components/order-confirmation.jsx
+++ client/components/order-confirmation.jsx
@@ -6,14 +6,14 @@ function OrderConfirmation(props) {
   } else {
     const cartOverview = props.confirmationPage.map(product => {
       return (
-        <div className="border rounded bg-white p-3 mb-3 d-flex" key={product.id}>
+        <div className="border rounded bg-white p-3 mb-3 d-flex slide-in" key={product.id}>
           <div className="col-4 p-0 mr-3">
-            <img className="image-fluid" src={product.image} />
+            <img className="image-fluid slide-in" src={product.image} />
           </div>
           <div className="my-auto">
-            <h3>{product.name}</h3>
-            <h6>${(product.price / 100).toFixed(2)}</h6>
-            <small className="text-muted">Quantity: {product.quantity}</small>
+            <h3 className="slide-in">{product.name}</h3>
+            <h6 className="slide-in">${(product.price / 100).toFixed(2)}</h6>
+            <small className="text-muted slide-in">Quantity: {product.quantity}</small>
           </div>
         </div>
       );
@@ -21,11 +21,11 @@ function OrderConfirmation(props) {
 
     return (
       <div className="container py-5">
-        <h1>Thank you for your order!</h1>
-        <h3>Your Order:</h3>
+        <h1 className="slide-in">Thank you for your order!</h1>
+        <h3 className="slide-in">Your Order:</h3>
         <div>
           {cartOverview}
-          <button className="btn text-white primary my-auto" onClick={() => {
+          <button className="btn text-white primary my-auto slide-in" onClick={() => {
             props.setView('catalog', {});
           }}>Continue Shopping</button>
         </div>
